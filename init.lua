f19391ace2c9989865c1214e4be002f767d2a03a
Samuel Simplicio <samuelsimplicio5@gmail.com>
2024-03-08 11:49:25 -0300

    Add lines yanked to clipboard

--- init.lua
+++ init.lua
@@ -374,7 +374,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust',
     'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
@@ -607,6 +607,7 @@ keymap("n", "<Down>", "<Nop>", opts)
 
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
+vim.api.nvim_set_option("clipboard","unnamedplus")
 
 -- Map Ctrl+hjkl to arrow keys in insert mode
 vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true })
