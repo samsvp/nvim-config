0c35b6af3d908c60b376158f9185f881a1d15843
Samuel Simplicio <samuelsimplicio5@gmail.com>
2024-07-22 01:56:02 -0300

    Update indent blankline version

--- init.lua
+++ init.lua
@@ -198,16 +198,7 @@ require('lazy').setup({
     end,
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
   {
     "mg979/vim-visual-multi",
