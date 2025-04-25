659eda4dd6b1586236e50be779f07c7315a462df
Samuel Simplicio <samuelsimplicio5@gmail.com>
2025-04-25 15:12:14 -0300

    JS TS Colors

--- init.lua
+++ init.lua
@@ -382,7 +382,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'c_sharp',
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'c_sharp', 'javascript',
     'tsx', 'typescript', 'vimdoc', 'vim', 'gleam', 'erlang', 'zig', 'glsl', 'fennel', 'templ' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
